8a6a6ab624f73487493c9bb88718d7d7cbd9426b
Alex Stokes <r.alex.stokes@gmail.com>
2026-07-25 17:27:43 -0600

    Optimize crane CI checks

--- nix/crane-checks.nix
+++ nix/crane-checks.nix
@@ -13,28 +13,27 @@ let
   craneLibStable = craneLib.overrideToolchain rustToolchain;
   craneLibNightly = craneLib.overrideToolchain nightlyToolchain;
   cargoSrc = pkgs.lib.cleanSourceWith {
-    src = src;
+    src = pkgs.lib.cleanSource src;
     filter =
-      path: _type:
-      let
-        relativePath = pkgs.lib.removePrefix "${toString src}/" (toString path);
-      in
-      !(
-        relativePath == ".git"
-        || pkgs.lib.hasPrefix ".git/" relativePath
-        || relativePath == "result"
-        || pkgs.lib.hasPrefix "result/" relativePath
-        || relativePath == "target"
-        || pkgs.lib.hasPrefix "target/" relativePath
-      );
+      path: type:
+      type == "directory"
+      || craneLib.filterCargoSources path type
+      || pkgs.lib.hasSuffix ".stderr" (toString path)
+      || pkgs.lib.hasSuffix "/assets/index.html" (toString path);
   };
   commonArgs = {
+    CARGO_PROFILE = "";
     pname = "tokio-otp";
     src = cargoSrc;
     strictDeps = true;
     version = "0.1.0";
   };
-  cargoArtifacts = craneLibStable.buildDepsOnly commonArgs;
+  dependencyArgs = commonArgs // {
+    cargoCheckExtraArgs = "";
+    cargoExtraArgs = "--locked --workspace --all-targets --all-features";
+  };
+  cargoArtifactsStable = craneLibStable.buildDepsOnly dependencyArgs;
+  cargoArtifactsNightly = craneLibNightly.buildDepsOnly dependencyArgs;
 in
 {
   cargo-fmt = craneLibNightly.cargoFmt (
@@ -47,57 +46,29 @@ in
   cargo-clippy = craneLibNightly.cargoClippy (
     commonArgs
     // {
-      inherit cargoArtifacts;
+      cargoArtifacts = cargoArtifactsNightly;
       cargoExtraArgs = "--locked";
       cargoClippyExtraArgs = "--workspace --all-targets --all-features -- -D warnings";
+      doInstallCargoArtifacts = false;
     }
   );
 
-  cargo-build = craneLibStable.cargoBuild (
+  cargo-ci = craneLibStable.mkCargoDerivation (
     commonArgs
     // {
-      inherit cargoArtifacts;
-      cargoExtraArgs = "--locked --workspace --all-targets --all-features";
-    }
-  );
-
-  cargo-test = craneLibStable.cargoTest (
-    commonArgs
-    // {
-      inherit cargoArtifacts;
-      cargoExtraArgs = "--locked --workspace --all-targets --all-features";
-    }
-  );
-
-  cargo-doctest = craneLibStable.cargoTest (
-    commonArgs
-    // {
-      inherit cargoArtifacts;
-      cargoExtraArgs = "--locked --workspace --doc --all-features";
-    }
-  );
-
-  examples-smoke = craneLibStable.mkCargoDerivation (
-    commonArgs
-    // {
-      inherit cargoArtifacts;
+      cargoArtifacts = cargoArtifactsStable;
       buildPhaseCargoCommand = ''
+        cargo build --locked --workspace --all-targets --all-features
+        cargo test --locked --workspace --all-targets --all-features
+        cargo test --locked --workspace --doc --all-features
         cargo run --locked -p tokio-otp --example trading_engine --features metrics
         cargo run --locked -p tokio-otp --example agent_control --features metrics
         cargo run --locked -p tokio-otp --example supervised_actors
         cargo run --locked -p tokio-otp --example ref_rebind
         cargo run --locked -p tokio-otp --example drain_policy
+        RUSTDOCFLAGS="-D warnings" cargo doc --locked --workspace --no-deps --all-features
       '';
-      installPhaseCommand = "true";
-    }
-  );
-
-  cargo-doc = craneLibStable.cargoDoc (
-    commonArgs
-    // {
-      inherit cargoArtifacts;
-      cargoExtraArgs = "--locked --workspace --all-features";
-      RUSTDOCFLAGS = "-D warnings";
+      doInstallCargoArtifacts = false;
     }
   );
 }
